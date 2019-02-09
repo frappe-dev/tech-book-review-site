8bcb489aa07b5c178892edc435d33ff91cd5ceda
松栄健太 <matuse.kenta@gmail.com>
2019-02-09 14:55:44 +0900

    複数のレビュー項目を表示して、コンソールに結果を表示するように設定

---
+++ src/components/EvaluationRadioButtons.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default class ReviewItem extends React.Component {
+
+
+    handleChangeRadio(event) {
+        this.props.updateState({ [event.target.name]: event.target.value });
+    }
+
+    render() {
+        return (
+          <label>{this.props.title}
+              <input type="radio" name={this.props.reviewKeyName} onChange={this.handleChangeRadio} value={1} />1
+              <input type="radio" name={this.props.reviewKeyName} onChange={this.handleChangeRadio} value={2} />2
+              <input type="radio" name={this.props.reviewKeyName} onChange={this.handleChangeRadio} value={3} />3
+              <input type="radio" name={this.props.reviewKeyName} onChange={this.handleChangeRadio} value={4} />4
+              <input type="radio" name={this.props.reviewKeyName} onChange={this.handleChangeRadio} value={5} />5
+          </label>
+        )
+    }
+}
