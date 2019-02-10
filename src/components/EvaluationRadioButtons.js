5f614ebb1a0d181f9c804445e03264bd5a4c0fcb
松栄健太 <matuse.kenta@gmail.com>
2019-02-10 14:17:47 +0900

    ラジオボタンをMaterialUIのものに変更

--- src/components/EvaluationRadioButtons.js
+++ src/components/EvaluationRadioButtons.js
@@ -1,24 +1,29 @@
 import React from 'react';
+import Radio from '@material-ui/core/Radio';
 
 export default class ReviewItem extends React.Component {
     constructor(props) {
         super(props);
         this.handleChangeRadio = this.handleChangeRadio.bind(this);
+		this.state = {
+            selectedValue: ''
+        };
     }
 
-    handleChangeRadio(event) {
-        this.props.updateState({ [event.target.name]: event.target.value });
+	handleChangeRadio(event) {
+		this.setState({ "selectedValue": event.target.value });
+		this.props.updateState({ [event.target.name]: event.target.value });
     }
 
     render() {
         return (
-          <label>{this.props.title}
-              <input type="radio" name={this.props.reviewKeyName} onChange={this.handleChangeRadio} value={1} />1
-              <input type="radio" name={this.props.reviewKeyName} onChange={this.handleChangeRadio} value={2} />2
-              <input type="radio" name={this.props.reviewKeyName} onChange={this.handleChangeRadio} value={3} />3
-              <input type="radio" name={this.props.reviewKeyName} onChange={this.handleChangeRadio} value={4} />4
-              <input type="radio" name={this.props.reviewKeyName} onChange={this.handleChangeRadio} value={5} />5
-          </label>
+          	<label>{this.props.title}
+				1<Radio checked={this.state.selectedValue === '1'} onChange={this.handleChangeRadio} value="1" name={this.props.reviewKeyName} aria-label="A" />
+				2<Radio checked={this.state.selectedValue === '2'} onChange={this.handleChangeRadio} value="2" name={this.props.reviewKeyName} aria-label="B" />
+				3<Radio checked={this.state.selectedValue === '3'} onChange={this.handleChangeRadio} value="3" name={this.props.reviewKeyName} aria-label="C" />
+				4<Radio checked={this.state.selectedValue === '4'} onChange={this.handleChangeRadio} value="4" name={this.props.reviewKeyName} aria-label="D" />
+				5<Radio checked={this.state.selectedValue === '5'} onChange={this.handleChangeRadio} value="5" name={this.props.reviewKeyName} aria-label="E" />
+          	</label>
         )
     }
 }
