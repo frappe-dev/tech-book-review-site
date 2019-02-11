e3d5cacd41c8f6887105eb613a6353670ea016a6
Kodai Takagi <mj5824@icloud.com>
2019-02-12 07:45:26 +0900

    作業用

--- src/ActionNameList.js
+++ src/ActionNameList.js
@@ -2,5 +2,6 @@ export const ActionNameList = {
     searchBookRequested : "SEARCH_BOOK_REQUESTED",
     searchBookSucceeded : "SEARCH_BOOK_SUCCEEDED",
     searchBookError     : "SEARCH_BOOK_ERROR",
-    reviewItems         : "REVIEW_ITEMS",
+    reviewBookRequested : "REVIEW_BOOK_REQUESTED",
+
 }
--- src/saga/rootSaga.js
+++ src/saga/rootSaga.js
@@ -3,6 +3,11 @@ import { ActionNameList } from '../ActionNameList';
 import axios from 'axios';
 
 const GOOGLE_BOOK_API_ENDPOINT = "https://www.googleapis.com/books/v1/volumes";
+const BOOKREVIEW_API_ENDPOINT  = "https://nd40yngtt7.execute-api.ap-northeast-1.amazonaws.com/dev/bookreview"; //　ソースコードで持たないほうがいい??
+
+function postBookReview(body) {
+    return;
+}
 
 function searchBook(keyword) {
     return axios.get(GOOGLE_BOOK_API_ENDPOINT, {
@@ -18,15 +23,27 @@ function searchBook(keyword) {
     })
 }
 
+function* handleReviewBook() {
+    while (true) {
+        const action = yield take(ActionNameList.reviewBookRequested);
+        const { result, err } = yield call(postBookReview, action.payload);
+        if (!err) {
+            console.log("succeed");
+            //yield put({type: ActionNameList.searchBookSucceeded, payload: result.data.items});
+        } else {
+            console.log("err is happened");
+            console.log(err);
+//            yield put({type: ActionNameList.searchBookError, isError: true});
+        }
+    }
+}
+
 function* handleSearchBook() {
     while (true) {
         const action = yield take(ActionNameList.searchBookRequested);
-        const { result, err } = yield call(searchBook, action.payload)
+        const { result, err } = yield call(searchBook, action.payload);
         if (!err) {
-            //yield put({type: ActionNameList.searchBookSucceeded, payload: JSON.stringify(result.data)});
-            
             yield put({type: ActionNameList.searchBookSucceeded, payload: result.data.items});
-            
         } else {
             console.log("err is happened");
             console.log(err);
@@ -38,4 +55,5 @@ function* handleSearchBook() {
 export default function* rootSaga() {
     // takeEveryにするかは要検討
     yield fork(handleSearchBook);
+    yield fork(handleReviewBook);
 }
