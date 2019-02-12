f407c9c6bee72292252203dccaa5da384025063c
fx30660 <mj5824@icloud.com>
2019-02-12 17:36:22 +0900

    for work

--- src/ActionNameList.js
+++ src/ActionNameList.js
@@ -2,6 +2,10 @@ export const ActionNameList = {
     searchBookRequested : "SEARCH_BOOK_REQUESTED",
     searchBookSucceeded : "SEARCH_BOOK_SUCCEEDED",
     searchBookError     : "SEARCH_BOOK_ERROR",
-    reviewBookRequested : "REVIEW_BOOK_REQUESTED",
-
+    postReviewRequested : "POST_REVIEW_REQUESTED",
+    postReviewSucceeded : "POST_REVIEW_SUCCEEDED",
+    postReviewError     : "POST_REVIEW_ERROR",
+    getReviewRequested  : "GET_REVIEW_REQUESTED",
+    getReviewSucceeded  : "GET_REVIEW_SUCCEEDED",
+    getReviewError      : "GET_REVIEW_EROOR",
 }
--- src/saga/rootSaga.js
+++ src/saga/rootSaga.js
@@ -5,8 +5,40 @@ import axios from 'axios';
 const GOOGLE_BOOK_API_ENDPOINT = "https://www.googleapis.com/books/v1/volumes";
 const BOOKREVIEW_API_ENDPOINT  = "https://nd40yngtt7.execute-api.ap-northeast-1.amazonaws.com/dev/bookreview"; //　ソースコードで持たないほうがいい??
 
-function postBookReview(body) {
-    return;
+function getReview(body) {
+    //XXX: body -> bookID
+    const bookID = "BOOK1";
+    return axios.get(BOOKREVIEW_API_ENDPOINT+"bookreview", {
+	params: {
+	    bookID = bookID
+	}
+    }).then(res => {
+	const result = res;
+	return { result }
+    }).catch(err => {
+	return { err }
+    })
+	    
+}
+
+function postReview(body) {
+    //XXX: parse params from body
+    return axios.post(BOOKREVIEW_API_ENDPOINT+"bookreview", {
+        bookID: "AAA",
+        userID: "AAA",
+        overallpoints: "4",
+        evaluation: {
+           param1: "3",
+           param2: "4",
+           param3: "3"
+        },
+        ISBN: "BBB"
+    }).then(res => {
+	const result = res;
+	return { result }
+    }).catch(err => {
+	return { err }
+    })
 }
 
 function searchBook(keyword) {
@@ -16,24 +48,39 @@ function searchBook(keyword) {
         }
     }).then(res => {
         //XXX: jsonの加工をする
-        const result = res
+        const result = res;
         return { result } //返して代入する変数名と合わせる
     }).catch(err => {
         return { err }
     })
 }
 
-function* handleReviewBook() {
+function* handleGetReview() {
     while (true) {
-        const action = yield take(ActionNameList.reviewBookRequested);
-        const { result, err } = yield call(postBookReview, action.payload);
+        const action = yield take(ActionNameList.getReviewRequested);
+        const { result, err } = yield call(getReview, action.payload);
         if (!err) {
             console.log("succeed");
-            //yield put({type: ActionNameList.searchBookSucceeded, payload: result.data.items});
+            //yield put({type: ActionNameList.getReviewSucceeded, payload: result.data.items});
         } else {
             console.log("err is happened");
             console.log(err);
-//            yield put({type: ActionNameList.searchBookError, isError: true});
+            yield put({type: ActionNameList.getReviewError, isError: true});
+        }
+    }
+}
+
+function* handlePostReview() {
+    while (true) {
+        const action = yield take(ActionNameList.reviewBookRequested);
+        const { result, err } = yield call(postReview, action.payload);
+        if (!err) {
+            console.log("succeed");
+            //yield put({type: ActionNameList.postReviewSucceeded, payload: result.data.items});
+        } else {
+            console.log("err is happened");
+            console.log(err);
+            yield put({type: ActionNameList.postReviewError, isError: true});
         }
     }
 }
@@ -55,5 +102,6 @@ function* handleSearchBook() {
 export default function* rootSaga() {
     // takeEveryにするかは要検討
     yield fork(handleSearchBook);
-    yield fork(handleReviewBook);
+    yield fork(handlePostReview);
+    yield fork(handleGetReview);
 }
