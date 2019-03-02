a4b072a526130889ef54225728e3906887619e3b
松栄健太 <matuse.kenta@gmail.com>
2019-03-02 23:45:57 +0900

    項目追加＋API叩いて問題ないところまで確認

---
+++ src/components/EvaluationSelector.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
+const styles = ({
+    selector: {
+        width: 350,
+    },
+});
+
+class EvaluationSelector extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChangeValue = this.handleChangeValue.bind(this);
+        this.state = {
+            selectedValue: ''
+        };
+    }
+
+    handleChangeValue(event) {
+        this.setState({ "selectedValue": event.target.value });
+        this.props.updateState({ [event.target.name]: event.target.value });
+    }
+
+    render() {
+        const { classes } = this.props;
+        return (
+            <FormControl className={classes.selector}>
+                <InputLabel htmlFor="age-simple">{this.props.label}</InputLabel>
+                <Select
+                    value={this.state.selectedValue}
+                    onChange={this.handleChangeValue}
+                    name={this.props.reviewKeyName}
+                >
+                    <MenuItem value={"初級者"}>初級者</MenuItem>
+                    <MenuItem value={"中級者"}>中級者</MenuItem>
+                    <MenuItem value={"上級者"}>上級者</MenuItem>
+                </Select >
+            </FormControl >
+        );
+    }
+}
+
+export default withStyles(styles)(EvaluationSelector);
--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -5,6 +5,9 @@ import { Auth } from 'aws-amplify';
 
 // component
 import EvaluationRadioButtons from '../components/EvaluationRadioButtons';
+import FreeWordInput from '../components/FreeWordInput'
+import EvaluationSelector from '../components/EvaluationSelector'
+
 // action
 import { postReviewRequested } from '../actions/ReviewActions';
 
@@ -16,7 +19,9 @@ class ReviewForm extends React.Component {
 			reviewPoint2: '',
 			reviewPoint3: '',
 			reviewPoint4: '',
-			reviewPoint5: ''
+			reviewPoint5: '',
+			reviewPoint6: '',
+			reviewPoint7: '',
 		};
 		this.updateState = this.updateState.bind(this);
 		this.onSubmit = this.onSubmit.bind(this);
@@ -25,16 +30,17 @@ class ReviewForm extends React.Component {
 	updateState(state) {
 		this.setState(state);
 		this.props.updateState(state);
+		console.log(this.state);
 	}
 
-	async onSubmit(){
+	async onSubmit() {
 		console.log(this.state);
 		// check isAuthenticated
 		let auth = false;
 		let userID = "";
 		await Auth.currentAuthenticatedUser()
-            .then(data => {
-                auth = true;
+			.then(data => {
+				auth = true;
 				console.log(data);
 				//console.log(data.attributes.sub) // -> Cognito User
 				//console.log(data.id) // -> facebook or google
@@ -45,21 +51,21 @@ class ReviewForm extends React.Component {
 				} else {
 					console.log("[ERROR]cannot parse userID"); // -> TODO: error throw
 				}
-            }).catch(err => {
-                auth = false;
-                console.log(err);
-            })
+			}).catch(err => {
+				auth = false;
+				console.log(err);
+			})
 		if (!auth) {
 			this.props.history.push('/login')
 			return;
 		}
-		console.log("userID: "+userID);
+		console.log("userID: " + userID);
 		let ISBN = this.props.ISBN;
 		if (!ISBN) {
 			ISBN = "-"
 		}
-		console.log("ISBN: "+ISBN);
-		console.log("BookID: "+this.props.bookID);
+		console.log("ISBN: " + ISBN);
+		console.log("BookID: " + this.props.bookID);
 		let params = {
 			bookID: this.props.bookID,
 			userID: userID,
@@ -70,8 +76,10 @@ class ReviewForm extends React.Component {
 				param3: this.state.reviewPoint3,
 				param4: this.state.reviewPoint4,
 				param5: this.state.reviewPoint5,
+				param6: this.state.reviewPoint6,
+				param7: this.state.reviewPoint7,
 			},
-			ISBN: ISBN
+			ISBN: ISBN,
 		}
 		this.props.postReview(params);
 	}
@@ -81,34 +89,46 @@ class ReviewForm extends React.Component {
 			<form>
 				<EvaluationRadioButtons
 					updateState={this.updateState}
-					title="ページ構成の読みやすさ"
+					title="文章の読みやすさ"
 					reviewKeyName="reviewPoint1"
-					/>
-				<br/>
+				/>
+				<br />
+				<EvaluationRadioButtons
+					updateState={this.updateState}
+					title="図や例の多さ"
+					reviewKeyName="reviewPoint2"
+				/>
+				<br />
 				<EvaluationRadioButtons
 					updateState={this.updateState}
 					title="内容の正確さ"
-					reviewKeyName="reviewPoint2"
-					/>
-				<br/>
-				<EvaluationRadioButtons
-					updateState={this.updateState}
-					title="サンプルが実行可能"
 					reviewKeyName="reviewPoint3"
-					/>
-				<br/>
+				/>
+				<br />
 				<EvaluationRadioButtons
 					updateState={this.updateState}
-					title="想定しているレベルは適切か"
+					title="全体的な満足度"
 					reviewKeyName="reviewPoint4"
-					/>
-				<br/>
-				<EvaluationRadioButtons
+				/>
+				<br />
+				<FreeWordInput
 					updateState={this.updateState}
-					title="内容の充実度"
 					reviewKeyName="reviewPoint5"
-					/>
-				<br/>
+					label="買った動機（任意）"
+				/>
+				<br />
+				<FreeWordInput
+					updateState={this.updateState}
+					reviewKeyName="reviewPoint6"
+					label="動機に適していたか（任意）"
+				/>
+				<br />
+				<EvaluationSelector
+					updateState={this.updateState}
+					reviewKeyName="reviewPoint7"
+					label="読んでほしい読者のレベル（任意）"
+				/>
+				<br />
 				<button type="button" name="submit" onClick={this.onSubmit}>送信</button>
 			</form>
 		)
