60a9bb65059f72c7f1e475d88d39190ddbe1e3f4
matsuei <matuse.kenta@gmail.com>
2019-04-03 19:25:54 +0900

    高木指摘箇所修正

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -10,28 +10,25 @@ class ErrorBoundary extends React.Component {
     }
 
     componentDidCatch(error, errorInfo) {
-        // errorInfoを改行で分けた時の3番目の要素にエラーが起きたコンポーネント名が入っている
-        let errorComponent = errorInfo.componentStack.split(/(\r\n|\n)/g)[2];
         this.setState({
             error: error,
             errorInfo: errorInfo,
-            errorComponent: errorComponent,
         });
     }
 
     render() {
-        if (this.state.error) {
+        if (this.state.errorInfo) {
             let errorMessage = "";
-            if (this.state.errorComponent) {
-                if (this.state.errorComponent.includes("Card")) {
-                    errorMessage = "書籍の情報取得に失敗しました";
-                } else {
-                    errorMessage = "ページの表示に失敗しました\nホームから再度アクセスしてください";
-                }
+            // errorInfoを改行で分けた時の3番目の要素にエラーが起きたコンポーネント名が入っている
+            let errorComponent = this.state.errorInfo.componentStack.split(/(\r\n|\n)/g)[2];
+            if (errorComponent.includes("Card")) {
+                errorMessage = "書籍の情報取得に失敗しました。";
+            } else {
+                errorMessage = "ページの表示に失敗しました。ホームから再度アクセスしてください。";
             }
-            return (errorMessage.split(/(\r\n|\n)/g).map((line, i) => {
-                return line.match(/(\r\n|\n)/g) ? <br key={i} /> : line
-            }));
+            return (
+                <h2>{ errorMessage }</h2>
+            );
         }
         // Normally, just render children
         return this.props.children;
